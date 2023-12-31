db01ad8114bf94e26baba16f51d8a884168a57e6
rjb25 <jason.basanese@gmail.com>
2023-12-31 15:25:19 -0500

    Basic working state with minimal graphics

--- card.py
+++ card.py
@@ -2,6 +2,7 @@ import asyncio
 import websockets
 import sys
 import requests
+import functools
 import json
 from fractions import Fraction
 from operator import itemgetter
@@ -10,7 +11,6 @@ import random
 import traceback
 from functools import reduce
 from operator import getitem
-from collections import defaultdict
 from collections import defaultdict
 import pprint
 import tkinter as tk
@@ -26,7 +26,7 @@ def geti(list, index, default_value):
         return list[index]
     except:
         return default_value
-
+#ddel(battleInfo,["groups",group])
 def ddel(context,path):
     deep = pathing(path[:-1],context)
     if isinstance(deep, list):
@@ -66,51 +66,142 @@ def dget(context, path, default=None):
         return internal_dict_value
     except:
         return default
-
-def weedNones(dictionary):
-    return {k:v for k,v in dictionary.items() if ((v is not None) and (v != [None])) and v != []}
                 
 def load(a):
     with open(a["file"]) as f:
             return json.load(f)
 
-def callLoad(a):
-    global cacheTable
-    directory = ""
-    if a.get("directory"):
-        directory = a["directory"]
-    for newFile in a["file"]:
-        creatureJson = load({"file": directory+"/"+newFile})
-        hasCategory = cacheTable.get(a["category"])
-        if hasCategory:
-            cacheTable[a["category"]][creatureJson["index"]] = creatureJson
-        else:
-            printw("Invalid category to load into")
-
-library_table = load({"file":"json/library.json"})
+decks_table = load({"file":"json/decks.json"})
 state_table = load({"file":"json/state.json"})
+adventure_table = load({"file":"json/adventure.json"})
+cards_table = load({"file":"json/cards.json"})
+situations_table = load({"file":"json/situations.json"})
+
 def saveBattle():
         with open('json/state.json', 'w') as f:
             json.dump(state_table,f)
+        with open('json/decks.json', 'w') as f:
+            json.dump(state_table,f)
 
+def add_dicts2(di1,di2):
+    return {i: di1.get(i, 0) + di2.get(i, 0)for i in set(di1).union(di2)}
 
-all_clients = []
+def add_dicts(dicts):
+    return functools.reduce(add_dicts2, dicts)
+
+def sub_dicts2(di1,di2):
+    return {i: di1.get(i, 0) - di2.get(i, 0)for i in set(di1).union(di2)}
+
+def sub_dicts(dicts):
+    return functools.reduce(sub_dicts2, dicts)
+
+def remaining_situation(situation):
+    for element,value in situation.items():
+        if value < 0:
+            situation[element] = value*-1;
+        if value > 0:
+            situation[element] = 0;
+    return situation
+
+def situation_handled(situation):
+    for element,value in situation.items():
+        if value < 0:
+            return False
+    return True
+
+#CARDS
+def attack():
+    state_table["force"] = state_table["force"] +1;
+
+#SITUATIONS
+
+#COMMUNICATIONS
+all_clients = [] 
+cards_played = []
+adventure = "intro"
+def add_situation():
+    situation_deck = decks_table[adventure]
+    random.shuffle(situation_deck)
+    situation = add_dicts([situations_table[situation_deck[0]], situations_table[situation_deck[1]]])
+    state_table["situations"].append(situation)
+
+def run_situation(situations, plans):
+    situation = situations.pop(0)
+    while plans:
+        print(plans)
+        plan = plans.pop(0)
+        print(plans)
+        print(plan)
+        print(bool(plans))
+        print(situation)
+        result = sub_dicts([plan,situation])
+        handled = situation_handled(result)
+        if handled:
+            plans.insert(0,result)
+            return 1
+        else:
+            situation = remaining_situation(result)
+    return -1
+
+def reset_state():
+    state_table["situations"] = []
+    state_table["plans"] = []
+    state_table["victory"] = 0
+ 
 
 async def send_message(message: str):
     for client in all_clients:
         await client.send(message)
 
 async def new_client_connected(client_socket, path):
+    #if messages = allclients.length: globals()["situation_name"]()
     print("New client connected!")
+    username = path[1:]
+    print("User: "+username)
     all_clients.append(client_socket)
 
+    #Card management
+    deck = decks_table[username]
+    random.shuffle(deck)
+    hand = deck[:5]
+    deck = deck[5:]
+    discard = []
+
+    await send_message(str({"state":state_table,"hand":hand,"text":"Welcome!"}))
     while True:
-        new_message = await client_socket.recv()
-        print("Client sent:", new_message)
-        await send_message(new_message)
+        message = await client_socket.recv()
+        print("Client sent:", message)
+
+        choice = int(message)
+        card = hand.pop(choice)
+        discard.append(card)
+
+        #Use discard if deck empty
+        if len(deck) <= 0:
+            deck = discard.copy()
+            random.shuffle(deck)
+            discard = []
+        if len(deck) > 0:
+            hand.insert(choice,deck.pop(0))
+
+        cards_played.append(card)
+        if len(cards_played) == 3:
+            plan = add_dicts([cards_table[cards_played[0]], cards_table[cards_played[1]], cards_table[cards_played[2]]])
+            state_table["plans"].append(plan)
+            cards_played.clear()
+            state_table["victory"] += run_situation(state_table["situations"],state_table["plans"])
+            if state_table["victory"] > 4:
+                await send_message({"text":"You win!"})
+                reset_state()
+            if state_table["victory"] < -4:
+                await send_message({"text":"You lose! Neener!"})
+                reset_state()
+            add_situation()
+            await send_message(str({"state":state_table,"hand":hand,"text":"Turn executed!"}))
 
 async def start_server():
     print("Server started!")
+    add_situation()
     await websockets.serve(new_client_connected, "localhost", 12345)
 
 if __name__ == '__main__':
--- websocket_client.js
+++ websocket_client.js
@@ -1,20 +1,92 @@
 document.addEventListener('DOMContentLoaded', function(){
-    socketname = prompt()
-    const websocketClient = new WebSocket("wss://"+socketname);
+    //socketname = prompt("WebSocketURL no http://")
+    socketname = "cc6e-108-31-158-123.ngrok-free.app"
+    username = prompt("Username:")
+    //username = "jason"
+    const websocketClient = new WebSocket("wss://"+socketname+"/"+username);
     //const websocketClient = new WebSocket("ws://localhost:12345/");
-    const messagesContainer = document.querySelector("#message_container");
-    const messageInput = document.querySelector("[name=message_input]");
-    const sendMessageButton = document.querySelector("[name=send_message_button]");
+    const messagesContainer = document.querySelector("#messages_container");
+    const situationsContainer = document.querySelector("#situations_container");
+    const plansContainer = document.querySelector("#plans_container");
+    const victoriesContainer = document.querySelector("#victories_container");
+    const card1 = document.querySelector("[name=card1]");
+    const card2 = document.querySelector("[name=card2]");
+    const card3 = document.querySelector("[name=card3]");
+    const card4 = document.querySelector("[name=card4]");
+    const card5 = document.querySelector("[name=card5]");
+    cardButtons = [card1,card2,card3,card4,card5];
     websocketClient.onopen = function(){
         console.log("Client connected!");
-        sendMessageButton.onclick = function(){
-            websocketClient.send(messageInput.value);
+        card1.onclick = function(){
+            websocketClient.send(0);
+            cardButtons[0].value = "fog";
+        };
+        card2.onclick = function(){
+            websocketClient.send(1);
+            cardButtons[1].value = "fog";
+        };
+        card3.onclick = function(){
+            websocketClient.send(2);
+            cardButtons[2].value = "fog";
+        };
+        card4.onclick = function(){
+            websocketClient.send(3);
+            cardButtons[3].value = "fog";
+        };
+        card5.onclick = function(){
+            websocketClient.send(4);
+            cardButtons[4].value = "fog";
         };
 
         websocketClient.onmessage = function(message){
-            const newMessage = document.createElement("div");
-            newMessage.innerHTML = message.data;
-            messagesContainer.appendChild(newMessage);
+            messageJson = JSON.parse(message.data.replace(/'/g, '"'));
+            messageText = messageJson["text"];
+            state = messageJson["state"];
+
+            const messageDiv = document.createElement("div");
+            messageDiv.innerHTML = messageText;
+
+            if(messageText){
+                messagesContainer.innerHTML = "";
+                messagesContainer.appendChild(messageDiv);
+            }
+
+            if(state){
+                situations = state["situations"];
+                plans = state["plans"];
+                victories = state["victory"];
+
+                if(victories){
+                    victoriesContainer.innerHTML = "";
+                    const victoryDiv = document.createElement("div");
+                    victoryDiv.innerHTML = victories;
+                    victoriesContainer.appendChild(victoryDiv);
+                }
+
+                if(situations){
+                    situationsContainer.innerHTML = "";
+                    situations.forEach((situation) => {
+                        const situationDiv = document.createElement("div");
+                        situationDiv.innerHTML = JSON.stringify(situation);
+                        situationsContainer.appendChild(situationDiv);
+
+                    });
+                }
+
+                if(plans){
+                    plansContainer.innerHTML = "";
+                    plans.forEach((plan) => {
+                        const planDiv = document.createElement("div");
+                        planDiv.innerHTML = JSON.stringify(plan);
+                        plansContainer.appendChild(planDiv);
+
+                    });
+                }
+            }
+
+            messageJson["hand"].forEach((card, index) => {
+                cardButtons[index].value = card;
+            });
         };
     };
 }, false);
