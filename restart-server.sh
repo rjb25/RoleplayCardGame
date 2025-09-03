d75d6e7ec4d50f23f55f88eec0b6597eb5ad008e
rjb25 <jason.basanese@gmail.com>
2025-09-03 17:32:39 -0400

    standard update

--- restart-server.sh
+++ restart-server.sh
@@ -1,2 +1,5 @@
+git add -u
+git commit -m "standard update"
+git push origin main
 ssh root@roleplaycardgame.com "pkill -f 'python card.py'"
 ssh root@roleplaycardgame.com "cd RoleplayCardGame; git pull origin main; source ./venv/bin/activate; nohup python card.py &"
