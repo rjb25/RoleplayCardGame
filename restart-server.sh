8ef13edb5072a5f4002de7004c1a651c3c28da1d
rjb25 <jason.basanese@gmail.com>
2025-12-17 22:05:10 -0500

    standard update

--- restart-server.sh
+++ restart-server.sh
@@ -1,7 +1,7 @@
 git add -u
 git commit -m "standard update"
 git push origin main
-ssh root@roleplaycardgame.com "pkill -f 'python card.py'"
+ssh root@roleplaycardgame.com "pkill -f 'card.py'"
 ssh root@roleplaycardgame.com "cd RoleplayCardGame; git stash"
 ssh root@roleplaycardgame.com "cd RoleplayCardGame; git pull origin main;"
 ssh root@roleplaycardgame.com "cd RoleplayCardGame; git stash pop"
